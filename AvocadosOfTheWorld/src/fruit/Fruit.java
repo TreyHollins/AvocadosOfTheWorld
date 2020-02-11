c615cb014be0332fc3adbc8ad05f0ad4b7e6ebf5
hollindv <hollindv@MCD210-PC-09.uccc.uc.edu>
2020-02-11 12:18:30 -0500

    Added constructors to Fruit and Avocado classes. Added code to main.

--- AvocadosOfTheWorld/src/fruit/Fruit.java
+++ AvocadosOfTheWorld/src/fruit/Fruit.java
@@ -11,5 +11,47 @@ package fruit;
  *
  */
 public abstract class Fruit {
-
+	private int weight; //in ounces
+	private float cost;
+	
+	/***
+	 * Constructor
+	 * @param weight Weight of the fruit
+	 * @param cost Cost of the fruit
+	 */
+	public Fruit(int weight, float cost) {
+		setWeight(weight);
+		setCost(cost);
+	}
+	/***
+	 * Get the weight 
+	 * @return weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
+	/***
+	 * Define the weight
+	 * @param weight
+	 */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	/***
+	 * Get the cost
+	 * @return cost
+	 */
+	public float getCost() {
+		return cost;
+	}
+	/***
+	 * Define the cost
+	 * @param cost
+	 */
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+	public String toString() {
+		return "The fruit has a weight of " + weight + " and a cost of " + cost;
+	}
 }
--- AvocadosOfTheWorld/src/main/Main.java
+++ AvocadosOfTheWorld/src/main/Main.java
@@ -4,10 +4,18 @@
  * 
  */
 package main;
- 
+
+import fruit.Fruit;
+import fruit.Avocado; 
+
 public class Main {
 
 	public static void main(String[] args) {
+		//Declare and instantiate a fruit called Avocajoe
+		//Fruit avocajoe = new Fruit(); //Cannot instantiate an abstract class. Be able to explain why we have abstract classes
+		
+		//Declare and instantiate an avocado named avocajoe
+		Avocado avocajoe = new Avocado(6, (float).50);
 		
 
 	}
